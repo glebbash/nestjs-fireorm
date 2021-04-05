2b8e316345f8f086297e0e774cb3e9fba59da98d
Gleb Bashkatov <glebbash@gmail.com>
2021-04-05 13:30:59 +0300

    chore: setup repo

---
+++ README.md
@@ -0,0 +1,16 @@
+# nestjs-fireorm
+
+[![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+[![Coverage Status](https://coveralls.io/repos/github/glebbash/nestjs-fireorm/badge.svg?branch=master)](https://coveralls.io/github/glebbash/nestjs-fireorm?branch=master)
+
+[Nest.js](https://nestjs.com)
+[Fireorm](https://fireorm.js.org)
+module
+
+Installation:
+
+```sh
+npm i nestjs-fireorm
+```
+
+<!-- TODO: examples -->
---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type Jest from '@jest/types';
+
+const config: Jest.Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  collectCoverageFrom: ['src/*.{js,ts}'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testResultsProcessor: 'jest-sonar-reporter',
+  coverageDirectory: '../.coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
